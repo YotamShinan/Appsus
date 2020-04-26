202cbd8a54cf0a7b12afb7f4cac74895c2180b95
YotamShinan <yshinan@gmail.com>
2020-04-26 20:24:18 +0300

    second commit. still no errors

--- apps/keep/cmps/CreateNote.jsx
+++ apps/keep/cmps/CreateNote.jsx
@@ -1,39 +1,39 @@
 
 
 
-const postsArr = [
-    { type: "img", title: "image title" },
-    { type: "txt", title: "text title" },
-    { type: "todo", title: "todo title" },
-    { type: "video", url: "http://www.google.com", title: "title" }
-];
+// const postsArr = [
+//     { type: "img", title: "image title" },
+//     { type: "txt", title: "text title" },
+//     { type: "todo", title: "todo title" },
+//     { type: "video", url: "http://www.google.com", title: "title" }
+// ];
 
-class App extends React.Component {
-    render() {
-        return postsArr.map(post => <DynamicAlboker post={post} />);
-    }
-}
+// class App extends React.Component {
+//     render() {
+//         return postsArr.map(post => <DynamicAlboker post={post} />);
+//     }
+// }
 
 
 
-const NoteCreator = () => {
-    state = {
-        type: "txt"
-    }
-    render() {
-        return (
-            <div onClick=({ type: "vdo" })>Video</div>
-           <div>{ this.state === "txt" && imgForm }</div>
-        )
-    }
-}
+// const NoteCreator = () => {
+//     state = {
+//         type: "txt"
+//     }
+//     render() {
+//         return (
+//             <div onClick=({ type: "vdo" })>Video</div>
+//            <div>{ this.state === "txt" && imgForm }</div>
+//         )
+//     }
+// }
 
-const imgForm = ()
+// const imgForm = ()
 
-const ImgCmp = ({ title }) => <div >{title}</div>;
+// const ImgCmp = ({ title }) => <div >{title}</div>;
 
-const TxtCmp = ({ title }) => <div>{title}</div>;
+// const TxtCmp = ({ title }) => <div>{title}</div>;
 
-const TodoCmp = ({ title }) => <div>{title}</div>;
+// const TodoCmp = ({ title }) => <div>{title}</div>;
 
-ReactDOM.render(<App />, document.getElementById("container"));
+// ReactDOM.render(<App />, document.getElementById("container"));
