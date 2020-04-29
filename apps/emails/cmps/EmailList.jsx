287911e3b18a938c8dcf2e62104a5dd01fdb9a5f
dbattat1 <dbattat1@gmail.com>
2020-04-29 17:28:06 +0300

    independence day wooo 2

--- apps/emails/cmps/EmailList.jsx
+++ apps/emails/cmps/EmailList.jsx
@@ -7,12 +7,12 @@ export function EmailList(props) {
         <React.Fragment>
             <div className="emails-inner-container flex column">
                 <div className="emails-sort-btn flex" onClick={props.onSort}><i className="fas fa-sort"></i></div>
-                
-                <ul className="emails-table clean-list">
-                    { props.emails.map( (email, idx) => 
-                    <EmailPreview key={ idx } email = {email} /> )}
-                </ul>  
+
+                <ul className="emails-list clean-list">
+                    {props.emails.map((email, idx) =>
+                        <EmailPreview key={idx} email={email} />)}
+                </ul>
             </div>
-        </React.Fragment>    
-            )
+        </React.Fragment>
+    )
 }
