77b78ce9157db746820c6714967f70e4649c90dc
YotamShinan <yshinan@gmail.com>
2020-04-29 20:53:28 +0300

    final commit. Appsus is ready

--- apps/emails/services/emailService.js
+++ apps/emails/services/emailService.js
@@ -13,84 +13,83 @@ export default {
     querySearch
 }
 
-const gDefaultEmails = [
-    {
-    id: util.makeId(),
-    subject: 'Your API key has been stolen!!',
-    body: 'Don\'t upload your Google Maps API key to GitHub dumbass',
-    isRead: false,
-    sentAt: 1588152030009,
-    sender: util.makeLoremSender(),
-    senderAddress: 'frauds@Google.com',
-    isStarred: false,
-    isTrash: false
+const gDefaultEmails = [{
+        id: util.makeId(),
+        subject: 'Your API key has been stolen!!',
+        body: 'Don\'t upload your Google Maps API key to GitHub dumbass',
+        isRead: false,
+        sentAt: 1588152030009,
+        sender: util.makeLoremSender(),
+        senderAddress: 'frauds@Google.com',
+        isStarred: false,
+        isTrash: false
     },
     {
-    id: util.makeId(),
-    subject: 'Don\'t forget to do a pishpull',
-    body: 'Before you go asleep let\'s do a push and pull, push and pull.\n Hey! Don\'t forget to do add and commit.',
-    isRead: false,
-    sentAt: 1588152030009,
-    sender: util.makeLoremSender(),
-    senderAddress: 'pushpull@github.org',
-    isStarred: false,
-    isTrash: false
+        id: util.makeId(),
+        subject: 'Don\'t forget to do a pishpull',
+        body: 'Before you go asleep let\'s do a push and pull, push and pull.',
+        isRead: false,
+        sentAt: 1588152030009,
+        sender: util.makeLoremSender(),
+        senderAddress: 'pushpull@github.org',
+        isStarred: false,
+        isTrash: false
     },
     {
-    id: util.makeId(),
-    subject: 'My best independence day ever!',
-    body: 'How didn\'t I do it before. It is such a pleasure.',
-    isRead: false,
-    sentAt: 1588152030009,
-    sender: util.makeLoremSender(),
-    senderAddress: 'loremIpsum@gmail.com',
-    isStarred: false,
-    isTrash: false
+        id: util.makeId(),
+        subject: 'My best independence day ever!',
+        body: 'How didn\'t I do it before. It is such a pleasure.',
+        isRead: false,
+        sentAt: 1588152030009,
+        sender: util.makeLoremSender(),
+        senderAddress: 'loremIpsum@gmail.com',
+        isStarred: false,
+        isTrash: false
     },
     {
-    id: util.makeId(),
-    subject: 'It is working!!! OMG!!!',
-    body: 'Let\'s tell Nevo! I can\'t believe this. We have an appsus!',
-    isRead: false,
-    sentAt: 1588152030002,
-    sender: util.makeLoremSender(),
-    senderAddress: 'nevo@gmail.com',
-    isStarred: false,
-    isTrash: false
+        id: util.makeId(),
+        subject: 'It is working!!! OMG!!!',
+        body: 'Let\'s tell Nevo! I can\'t believe this. We have an appsus!',
+        isRead: false,
+        sentAt: 1588152030002,
+        sender: util.makeLoremSender(),
+        senderAddress: 'nevo@gmail.com',
+        isStarred: false,
+        isTrash: false
     },
     {
-    id: util.makeId(),
-    subject: 'Wassap?',
-    body: 'Pick up!',
-    isRead: true,
-    sentAt: 1551133930594,
-    sender: util.makeLoremSender(),
-    senderAddress: 'address@gmail.com',
-    isStarred: true,
-    isTrash: true
-},
-{
-    id: util.makeId(),
-    subject: 'manshma?',
-    body: 'hello there!',
-    isRead: false,
-    sentAt: 1551133930594,
-    sender: util.makeLoremSender(),
-    senderAddress: 'my@gmail.com',
-    isStarred: false,
-    isTrash: false
-},
-{
-    id: util.makeId(),
-    subject: 'New Email',
-    body: 'Done at createEmail a loooong time ago',
-    isRead: false,
-    sentAt: 155118390594,
-    sender: util.makeLoremSender(),
-    senderAddress: 'stam@gmail.com',
-    isStarred: false,
-    isTrash: false
-}
+        id: util.makeId(),
+        subject: 'Wassap?',
+        body: 'Pick up!',
+        isRead: true,
+        sentAt: 1551133930594,
+        sender: util.makeLoremSender(),
+        senderAddress: 'address@gmail.com',
+        isStarred: true,
+        isTrash: true
+    },
+    {
+        id: util.makeId(),
+        subject: 'manshma?',
+        body: 'hello there!',
+        isRead: false,
+        sentAt: 1551133930594,
+        sender: util.makeLoremSender(),
+        senderAddress: 'my@gmail.com',
+        isStarred: false,
+        isTrash: false
+    },
+    {
+        id: util.makeId(),
+        subject: 'New Email',
+        body: 'Done at createEmail a loooong time ago',
+        isRead: false,
+        sentAt: 155118390594,
+        sender: util.makeLoremSender(),
+        senderAddress: 'stam@gmail.com',
+        isStarred: false,
+        isTrash: false
+    }
 ]
 
 
@@ -99,9 +98,9 @@ var gEmails = null;
 
 createEmails();
 
-function createEmails(){
+function createEmails() {
     gEmails = storageService.loadFromStorage(STORAGE_KEY);
-    if (!gEmails || !gEmails.length){
+    if (!gEmails || !gEmails.length) {
         gEmails = gDefaultEmails;
         storageService.saveToStorage(STORAGE_KEY, gEmails)
     }
@@ -115,13 +114,13 @@ function query(filter) {
     return Promise.resolve(gEmails);
 }
 
-function querySearch(filterBy){
+function querySearch(filterBy) {
     let filter = filterBy.toLowerCase();
-    let emails = gEmails.filter(email =>   
-                                            (email.subject.toLowerCase().includes(filter) || 
-                                            email.body.toLowerCase().includes(filter) ||
-                                            email.sender.toLowerCase().includes(filter))
-                                        )
+    let emails = gEmails.filter(email =>
+        (email.subject.toLowerCase().includes(filter) ||
+            email.body.toLowerCase().includes(filter) ||
+            email.sender.toLowerCase().includes(filter))
+    )
     return Promise.resolve(emails);
 }
 
